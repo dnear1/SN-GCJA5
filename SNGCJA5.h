624db3fefb5e819dc422ca72ad0a8344ab847be9
D Near <dan.e.near@gmail.com>
2022-01-02 20:25:48 -0600

    Update SNGCJA5.h
    Added notes

--- SNGCJA5.h
+++ SNGCJA5.h
@@ -1,3 +1,38 @@
+ /*
+  This is a library written for the Panasonic SN-GCJA5 particle sensor
+  SparkFun sells these at its website: www.sparkfun.com
+  Do you like this library? Help support open source hardware. Buy a board!
+  https://www.sparkfun.com/products/17123
+
+  Written by Daniel Near Jan 2nd, 2022
+  
+  This library integrates the Sparkfun library for use in ESPHome
+  
+  SFE Library:
+  https://github.com/sparkfun/SparkFun_Particle_Sensor_SN-GCJA5 
+
+MIT License
+
+Copyright (c) 2022 D Near
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 #include "esphome.h"
 #include "SparkFun_Particle_Sensor_SN-GCJA5_Arduino_Library.h"
 //https://github.com/sparkfun/SparkFun_Particle_Sensor_SN-GCJA5_Arduino_Library
